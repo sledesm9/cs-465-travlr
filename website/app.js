818360105e896bf2b1b2c6ed931a9577fddb4198
Stevan Ledesma <stevanledesma@Stevans-MacBook-Pro.local>
2026-01-17 09:35:23 -0600

    Module 2: MVC routing and Handlebars templating

--- website/app.js
+++ website/app.js
@@ -1,11 +1,44 @@
-const express = require("express");
-const path = require("path");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const app = express();
-const port = 3000;
+// UPDATED PATHS (routes now live in app_server)
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./app_server/routes/users');
 
-app.use(express.static(path.join(__dirname, "public")));
+var app = express();
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+// view engine setup
+// UPDATED PATH (views now live in app_server)
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// public folder stays where it is
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ website/app_server/controllers/travel.js
@@ -0,0 +1,8 @@
+const travel = (req, res) => {
+    res.render('travel', { title: 'Travlr Getaways - Travel' });
+  };
+  
+  module.exports = {
+    travel
+  };
+  
